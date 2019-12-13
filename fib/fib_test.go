8ad7a74f3fadab226399c108b4adf10257660426
Karl McGuire <karl@xba.io>
2019-12-13 17:23:04 -0500

    testing

--- fib/fib_test.go
+++ fib/fib_test.go
@@ -2,12 +2,19 @@ package fib
 
 import (
 	"fmt"
+	"hash/fnv"
 	"testing"
 )
 
 func TestHash(t *testing.T) {
-	for i := 0; i < 16; i++ {
-		fmt.Printf("%2d: %d\n", i, Hash(uint64(i))>>61)
+	algo := fnv.New64a()
+	for i := uint64(0); i < 5; i++ {
+		algo.Write([]byte(fmt.Sprintf("%d", i)))
+		hash := algo.Sum64()
+		fmt.Printf("\t%2d: %d -> %d\n", i, hash, Hash(hash)>>48)
+		fmt.Printf("\t  : %d -> %d\n", hash<<16>>48, Hash(hash<<16>>48)>>48)
+		fmt.Printf("\t  : %d -> %d\n", hash<<32>>48, Hash(hash<<32>>48)>>48)
+		fmt.Printf("\t  : %d -> %d\n", hash<<48>>48, Hash(hash<<48>>48)>>48)
 	}
 }
 
